db95bf8e4565121ff645ebcf3b08ff8756559432
andesonatasha <andesonatasha@gmail.com>
2026-02-04 13:22:09 +0300

    added lesson1b

---
+++ lesson1b.py
@@ -0,0 +1,17 @@
+# Python Data types
+# String - It is a series of characters that are inside of quotes
+name = "Joseph Kainembuga"
+print(name)
+print(type(name))
+
+# Numeric
+# Int - this indicates a whole number in python. For an integer, we don't use quotes
+age = 39
+print("The age of a person is : ", age)
+print(type(age))
+
+# float - This a number with a decimal
+coordinates = 15.4524
+print("The coordinates are: ", coordinates)
+print(type(coordinates))
+
