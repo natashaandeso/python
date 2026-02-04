2bf3878f86e511a1099b9d4c7829e13e45b259f6
andesonatasha <andesonatasha@gmail.com>
2026-02-04 14:43:33 +0300

    added assignment

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+# python program that is able to calculate the BMI of a person whose weight is 78kg and height is 1.75 meters . (BMI =(weight)/ (height squared)).
+weight = 78
+height = 1.75
+BMI = (weight / height * 2)
+print("The BMI is:", BMI)
+
+# The area of a rectangle whose length is 48cm and width is 25cm
+length = 48
+width = 25
+Area = (length * width)
+print("The Area of the numbers is: ", Area)
+
+#  The perimeter of a rectangle whose length is 48cm and width is 25cm
+length = 48
+width = 25
+Perimeter = ((length + width) *2)
+print("The perimeter of this number is: ", Perimeter)
---
+++ lesson1d.py
@@ -0,0 +1,6 @@
+# Create a python program that is able to calculate the simple intrest given the principal as 50000, rate as 5% and time as 8 years. SI = (p * R * t)/100
+principal = 50000
+rate = 5
+time = 8
+simpleinterest = (principal * rate * time) / 100
+print("The simple interest is: ", simpleinterest)
