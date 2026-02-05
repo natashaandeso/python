df21ea1501ab4750fc04e14d9f9aaf283f0d33ea
andesonatasha <andesonatasha@gmail.com>
2026-02-05 15:07:11 +0300

    added assignment2

--- assignment2.py
+++ assignment2.py
@@ -1,11 +1,8 @@
 # Create a tuple of 8 different planets
 Planets = {
-    "Mars",
-    "Jupiter",
-    "Earth",
-    "Saturn",
-    "Uranus",
-    "Mercury",
-    "Venus",
-    "Neptune"
+    "names" : ["Mars", "Jupiter","Earth","Saturn", "Uranus", "Mercury","Venus", "Neptune"]
 }
+print(Planets)
+
+# print the planets from 2 to 6
+print(Planets["names"][2:6])
