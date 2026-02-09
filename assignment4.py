d7b21d73a80730e6151f4873b7d3987acdf1142a
andesonatasha <andesonatasha@gmail.com>
2026-02-09 16:30:05 +0300

    added assignment file

---
+++ assignment4.py
@@ -0,0 +1,22 @@
+# years = 2000
+
+for years in range(2000, 2024):
+    print(years)
+
+
+print('============================================')
+# a loop of colours
+colours = ["Blue", "Green", "Red", "Pink", "Black"]
+
+for colour in colours:
+    print(colour)
+
+print('============================================')
+
+# Using a while loop
+number = 20
+while number >= 1:
+    print(number)
+    number -=1
+    
+    
