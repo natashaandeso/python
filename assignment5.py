3dc3644bfb765ef7cab3564cf435ef21d6368001
andesonatasha <andesonatasha@gmail.com>
2026-02-10 15:57:49 +0300

    added files

---
+++ assignment5.py
@@ -0,0 +1,35 @@
+def area():
+    num1 = 4
+    num2 = 5
+    area=num1 * num2 
+    print("The area of a triangle is: ", area)
+
+area()
+
+print("==============================")
+def my_function(x, y):
+    sum = x + y
+    difference = x - y
+    product = x * y
+    quantine = x / y
+    print(f"The sum of the numbers is: {sum}")
+    print(f"The difference of the numbers is: {difference}")
+    print(f"The product of the numbers is: {product}")
+    print(f"The quantine of the numbers is: {quantine}")
+my_function(6, 24)
+
+print("==============================")
+
+number = int(input("Enter number: "))
+
+if number > 0:
+    print("positive")
+elif number == 0:
+    print("zero")
+else:
+    print("negative")
+
+
+print("==============================")
+
+
