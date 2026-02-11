678b3c556e6ba7afb2454b66ecaca502fcdea813
andesonatasha <andesonatasha@gmail.com>
2026-02-11 11:43:30 +0300

    added file

--- assignment5.py
+++ assignment5.py
@@ -16,6 +16,7 @@ def my_function(x, y):
     print(f"The difference of the numbers is: {difference}")
     print(f"The product of the numbers is: {product}")
     print(f"The quantine of the numbers is: {quantine}")
+
 my_function(6, 24)
 
 print("==============================")
@@ -32,4 +33,21 @@ else:
 
 print("==============================")
 
+def sum_of_numbers(n):
+    sum= 0
+    for i in range(1, n+1):
+         sum+=i
+    print("The sum of numbers from 1 to",n,"is:",sum)
+n=int(input("Enter a number: "))
+sum_of_numbers(n)
+print('=============================')
 
+
+def square_of_numbers(n):
+    i=1
+    while i<=n:
+        square=i**2
+        print("The square of",i,"is:",square) 
+        i+=1
+n=int(input("Enter a number: "))
+square_of_numbers()
